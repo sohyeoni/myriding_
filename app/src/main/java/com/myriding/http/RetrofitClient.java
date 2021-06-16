fcbbb2e5df763bbaf5ef1dbb9df770295e2421d2
sohyeon <sohyeon.kk@gmail.com>
2021-06-16 22:27:03 +0900

    retrofit url gitignore

--- app/src/main/java/com/myriding/http/RetrofitClient.java
+++ app/src/main/java/com/myriding/http/RetrofitClient.java
@@ -10,7 +10,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "http://54.145.82.27/api/";
+    private static final String BASE_URL = new ServerURL().getBaseUrl();
 
     public static RetrofitAPI getApiService(){ return getInstance().create(RetrofitAPI.class); }
 
---
+++ app/src/main/java/com/myriding/http/ServerURL.java
@@ -0,0 +1,9 @@
+package com.myriding.http;
+
+public class ServerURL {
+    private static final String BASE_URL = "http://54.145.82.27/api/";
+
+    public String getBaseUrl() {
+        return this.BASE_URL;
+    }
+}
